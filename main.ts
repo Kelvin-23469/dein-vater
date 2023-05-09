bcf1a557c01073a6da82a4dfa1dc86c927da9834
Kelvin-23469 <132395943+Kelvin-23469@users.noreply.github.com>
2023-05-09 10:24:58 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    basic.showString("Du Hund ")
+})
+input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+    basic.showString("Du Kelb")
+})
+basic.forever(function () {
+	
+})
